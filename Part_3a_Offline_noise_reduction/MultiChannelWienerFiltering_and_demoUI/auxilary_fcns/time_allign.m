197a477df2ca8fa42e1f794bb8b7f38d3047c758
floris.janssens <floris.janssens@student.kuleuven.be>
2019-12-13 05:29:46 +0100

    Final update MWF

--- Part_3a_Offline_noise_reduction/MultiChannelWienerFiltering_and_demoUI/auxilary_fcns/time_allign.m
+++ Part_3a_Offline_noise_reduction/MultiChannelWienerFiltering_and_demoUI/auxilary_fcns/time_allign.m
@@ -10,26 +10,26 @@ function [y_sd] = time_allign(y_sd1,y_sd2)
     if delay == 0
         if L1 > L2  % y_sd2 is shorter when delayed
             % recompensate the second signal ending 
-            y_sd = [y_sd2(1:end); zeros(L1 - L2 -1,1)];
+            y_sd = [y_sd2(1:end); zeros(L1 - L2 ,1)];
         else
             y_sd = y_sd2(1:L1);
         end
         
     elseif delay > 0 % y_sd2 needs to be shifted left with delay
-        if L1 > L2 - delay  % y_sd2 is shorter when delayed
+        if L1 >= L2 - delay  % y_sd2 is shorter when delayed
             % recompensate the second signal ending 
-            y_sd = [y_sd2(delay:end); zeros((L1 - (L2 - delay) -1),1)];
+            y_sd = [y_sd2(delay+1:end); zeros((L1 - (L2 - delay)),1)];
         else
-            y_sd = y_sd2(delay:L1+delay-1);
+            y_sd = y_sd2(delay+1:L1+delay);
         end
     
     else
         delay = abs(delay); % y_sd2 needs to be shifted right with delay
-        if L1 > L2 + delay  % y_sd2 is shorter even when delayed 
+        if L1 >= L2 + delay  % y_sd2 is shorter even when delayed 
             % Compensate the second signal ending
-            y_sd = [zeros(abs(delay),1); y_sd2; zeros((L1 - (L2 + delay)),1)];
+            y_sd = [zeros(delay,1); y_sd2; zeros((L1 - (L2 + delay)),1)];
         else                % y_sd2 is longer so only zero-pad the beginning
-            y_sd = [zeros(abs(delay),1); y_sd2(delay:L1-1)];
+            y_sd = [zeros(delay,1); y_sd2(1:L1-delay)];
         end
     end
     end
